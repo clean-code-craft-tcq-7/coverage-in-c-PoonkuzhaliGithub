cfb6b6a9cdb4cb31f436df5bbe14afafa9951164
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-01-11 21:36:40 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -7,36 +7,28 @@ int Battery_TempLimit[NUMBER_OF_BATTERY_TYPE][6] =
 {PASSIVE_COOLING_LOW_LIMIT,  PASSIVE_COOLING_HIGH_LIMIT,  MED_ACTIVE_COOLING_LOW_LIMIT,  MED_ACTIVE_COOLING_HIGH_LIMIT,  HI_ACTIVE_COOLING_LOW_LIMIT,  HI_ACTIVE_COOLING_HIGH_LIMIT};
 
 BreachType inferBreach(float value, float lowerLimit, float upperLimit) {
-  return checkBreach(value, lowerLimit, upperLimit);
-}
+  return checkBreach(value, lowerLimit, upperLimit);}
 
 BreachType classifyTemperatureBreach(BatteryCharacter batteryChar, float temperatureInC) {
   int LowLimit = batteryChar.coolingType;
   int UpperLimit = LowLimit + 1;
   int BatteryBrand = batteryChar.brand;
-  return inferBreach(temperatureInC, Battery_TempLimit[BatteryBrand][LowLimit], Battery_TempLimit[BatteryBrand][UpperLimit]);
-}
+  return inferBreach(temperatureInC, Battery_TempLimit[BatteryBrand][LowLimit], Battery_TempLimit[BatteryBrand][UpperLimit]);}
 
 void checkAndAlert(BatteryCharacter batteryChar, float temperatureInC, void (*alterType) (BreachType getbreachType)) {
   breachType = classifyTemperatureBreach(batteryChar, temperatureInC);
-  
-  (*alterType) (breachType);
-}
+  (*alterType) (breachType);}
 
 void sendToController_forTesting(BreachType getbreachType) {
-  SEND_MSG_TO_CONTROLLER(getbreachType);
-}
+  SEND_MSG_TO_CONTROLLER(getbreachType);}
 
 void sendToEmail_forTesting(BreachType getbreachType) {
-  SEND_MSG_THROUGH_EMAIL(getbreachType); 
-}
+  SEND_MSG_THROUGH_EMAIL(getbreachType); }
 
 void sendToController(BreachType getbreachType) {
   //will be processed in real time hw setup
-  SEND_MSG_TO_CONTROLLER(getbreachType);
-}
+  SEND_MSG_TO_CONTROLLER(getbreachType);}
 
 void sendToEmail(BreachType getbreachType) {
   //will be processed in real time
-  SEND_MSG_THROUGH_EMAIL(getbreachType);
-}
+  SEND_MSG_THROUGH_EMAIL(getbreachType);}
