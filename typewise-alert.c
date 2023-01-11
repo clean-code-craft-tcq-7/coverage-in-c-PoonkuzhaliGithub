c71cd0f407676ba5b5bc7e35b567a846998e9e80
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-01-11 21:23:39 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -6,18 +6,18 @@ BreachType breachType;
 int Battery_TempLimit[NUMBER_OF_BATTERY_TYPE][6] = 
 {PASSIVE_COOLING_LOW_LIMIT,  PASSIVE_COOLING_HIGH_LIMIT,  MED_ACTIVE_COOLING_LOW_LIMIT,  MED_ACTIVE_COOLING_HIGH_LIMIT,  HI_ACTIVE_COOLING_LOW_LIMIT,  HI_ACTIVE_COOLING_HIGH_LIMIT};
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+BreachType inferBreach(float value, float lowerLimit, float upperLimit) {
   return checkBreach(value, lowerLimit, upperLimit);
 }
 
-BreachType classifyTemperatureBreach(BatteryCharacter batteryChar, double temperatureInC) {
+BreachType classifyTemperatureBreach(BatteryCharacter batteryChar, float temperatureInC) {
   int LowLimit = batteryChar.coolingType;
   int UpperLimit = LowLimit + 1;
   int BatteryBrand = batteryChar.brand;
   return inferBreach(temperatureInC, Battery_TempLimit[BatteryBrand][LowLimit], Battery_TempLimit[BatteryBrand][UpperLimit]);
 }
 
-void checkAndAlert(BatteryCharacter batteryChar, double temperatureInC, void (*alterType) (BreachType getbreachType)) {
+void checkAndAlert(BatteryCharacter batteryChar, float temperatureInC, void (*alterType) (BreachType getbreachType)) {
   breachType = classifyTemperatureBreach(batteryChar, temperatureInC);
   
   (*alterType) (breachType);
