268882d81f87f6d7f187224fe60706daddeab72a
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-05 12:19:11 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "string.h"
 
 typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
+  SW_FOR_PRODUCTION,
+  SW_FOR_TESTING,
+  NUMBER_OF_SW
+} swType;
+
+typedef enum {
+  PASSIVE_COOLING = 0,
+  MED_ACTIVE_COOLING = 2,
+  HI_ACTIVE_COOLING = 4,
 } CoolingType;
 
 typedef enum {
@@ -12,21 +19,65 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
 } AlertTarget;
 
+typedef enum {
+  LI_ION_BATTERY,
+  NUMBER_OF_BATTERY_TYPE
+} BatteryType;
+
 typedef struct {
   CoolingType coolingType;
-  char brand[48];
+  BatteryType brand;
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+extern BreachType breachType;
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+#define CONTROLLER_ID                       0xfeed
+#define EMAIL_ID                            "ab.com"
+
+#define PASSIVE_COOLING_LOW_LIMIT           0
+#define PASSIVE_COOLING_HIGH_LIMIT          35
+#define HI_ACTIVE_COOLING_LOW_LIMIT         0
+#define HI_ACTIVE_COOLING_HIGH_LIMIT        45
+#define MED_ACTIVE_COOLING_LOW_LIMIT        0
+#define MED_ACTIVE_COOLING_HIGH_LIMIT       40
+
+#define checkBreach(currentvalue, lowerLimit, upperLimit)               \
+({                                                                      \
+  BreachType currentBreach = NORMAL;                                    \
+  if (value < lowerLimit) {                                             \
+     currentBreach = TOO_LOW;                                           \
+  }else if (value > upperLimit) {                                       \
+    currentBreach = TOO_HIGH;                                           \
+  }else {                                                               \
+  }                                                                     \
+  currentBreach;                                                        \
+})                                                                      \
+
+#define SEND_MSG_TO_CONTROLLER(breachType)                              \
+({printf("%x: %x\n",CONTROLLER_ID, breachType);})                                                                    
+
+#define SEND_MSG_THROUGH_EMAIL(breachType)                              \
+({printf("%s\n",EMAIL_ID);                                              \
+if (TOO_LOW == breachType)                                              \
+{printf("Hi, the temperature is too low\n");}                           \
+else if(TOO_HIGH == breachType)                                         \
+{printf("Hi, the temperature is too high\n");}                          \
+else                                                                    \
+{/* nothing to do */                                                    \
+}                                                                       \
+})                                                                      \
+
+void checkAndAlert(BatteryCharacter batteryChar, double temperatureInC, void (*alterType) (BreachType getbreachType));
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(BatteryCharacter batteryChar, double temperatureInC);
+
+void sendToController(BreachType getbreachType);
+void sendToEmail(BreachType getbreachType);
+void sendToController_forTesting(BreachType getbreachType);
+void sendToEmail_forTesting(BreachType getbreachType);
