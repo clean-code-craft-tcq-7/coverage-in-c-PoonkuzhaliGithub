5c4a5842cf351bafe58ddcf4db1ca7096915dec1
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2023-02-05 12:14:15 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,5 +4,39 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = PASSIVE_COOLING;
+  batteryChar.brand = LI_ION_BATTERY;
+  
+  checkAndAlert(batteryChar, -5, sendToController_forTesting);
+  REQUIRE(breachType == TOO_LOW);
+  
+  checkAndAlert(batteryChar, 35, sendToEmail_forTesting);
+  REQUIRE(breachType == NORMAL);
+  
+  checkAndAlert(batteryChar, 39, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_HIGH);
+  
+  checkAndAlert(batteryChar, -7, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_LOW);
+  
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  checkAndAlert(batteryChar, 40, sendToEmail_forTesting);
+  REQUIRE(breachType == NORMAL);
+  
+  checkAndAlert(batteryChar, 44, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_HIGH);
+  
+  checkAndAlert(batteryChar, -1, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_LOW);
+  
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(batteryChar, 45, sendToEmail_forTesting);
+  REQUIRE(breachType == NORMAL);
+  
+  checkAndAlert(batteryChar, 46, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_HIGH);
+  
+  checkAndAlert(batteryChar, -5, sendToEmail_forTesting);
+  REQUIRE(breachType == TOO_LOW);
 }
